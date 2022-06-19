981b40ff837db4bba1ab1cf8910539124a3192e7
alevcoder <alevcoder1@gmail.com>
2022-06-19 17:40:24 +0500

    url

--- academy/app/urls.py
+++ academy/app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [
     path('', fronthome, name='fronthome'),
+    path('about/', aboutpage, name='about'),
+    path('contact/', contactpage, name='contact'),
 ]
--- academy/app/views.py
+++ academy/app/views.py
@@ -5,3 +5,11 @@ from .models import News
 def fronthome(request):
     cours = News.objects.all()
     return render(request, 'index.html', {'Course': cours})
+
+
+def aboutpage(request):
+    return render(request, 'about-us.html')
+
+
+def contactpage(request):
+    return render(request, 'contact.html')
